8d6538ec27cc593826dffae93930c092a80fa1a2
Vlad <fencingmylove@gmail.com>
2022-10-06 17:07:32 +0300

    commit

---
+++ src/main/java/com/pbvz/springboot/app/springboot_blog/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.pbvz.springboot.app.springboot_blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "index";
+    }
+}
---
+++ src/test/java/com/pbvz/springboot/app/springboot_blog/SpringBootBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbvz.springboot.app.springboot_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
